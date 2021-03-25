5ea1cd94a60b5c979441c008040e1d03f2b89006
Arpit Bhardwaj <arpitbhardwaj@Arpits-MacBook-Pro.local>
2021-03-25 23:17:02 +0530

    Board created

--- src/App.js
+++ src/App.js
@@ -2,30 +2,56 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [delta, setDelta] = useState(1);
-  const increase = () => {
-    setCounter(counter + delta);
-  };
-  const decrease = () => {
-    if (counter > delta) {
-      setCounter(counter - delta);
-    }
-  };
-  const updateCounter = (e) => {
-    setDelta(parseInt(e.target.value));
+function Square(props) {
+  const [val, setVal] = useState(props.board[props.row - 1][props.col - 1]);
+  const onPress = () => {
+    console.log("Hey");
+    const newBoard = props.board;
+    newBoard[props.row - 1][props.col - 1] = "X";
+    setVal("X");
+    console.log(newBoard);
+    props.setBoard(newBoard);
+    console.log(props.board);
   };
   return (
-    <div className="App">
-      <div style={{ fontSize: 50 }}>Counter</div>
-      <div>This is second</div>
-      <div>{counter}</div>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <div>
-        <input onChange={updateCounter} />
-      </div>
+    <div
+      style={{
+        height: "100px",
+        width: "100px",
+        border: "1px solid black",
+        background: "#bbb",
+        fontSize: 72,
+        textAlign: "center",
+      }}
+      onClick={onPress}
+    >
+      {val}
+    </div>
+  );
+}
+
+function Row(props) {
+  const { board, setBoard, row, col } = props;
+  return (
+    <div className="Row">
+      <Square board={board} setBoard={setBoard} row={row} col={1} />
+      <Square board={board} setBoard={setBoard} row={row} col={2} />
+      <Square board={board} setBoard={setBoard} row={row} col={3} />
+    </div>
+  );
+}
+
+function App() {
+  const [board, setBoard] = useState([
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ]);
+  return (
+    <div>
+      <Row board={board} setBoard={setBoard} row={1} />
+      <Row board={board} setBoard={setBoard} row={2} />
+      <Row board={board} setBoard={setBoard} row={3} />
     </div>
   );
 }
