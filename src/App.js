ec3cdc4ee93b94849ec4d82ad980dfb45a76a27e
Arpit Bhardwaj <arpitbhardwaj@Arpits-MacBook-Pro.local>
2021-03-25 22:01:06 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [delta, setDelta] = useState(1);
+  const increase = () => {
+    setCounter(counter + delta);
+  };
+  const decrease = () => {
+    if (counter > delta) {
+      setCounter(counter - delta);
+    }
+  };
+  const updateCounter = (e) => {
+    setDelta(parseInt(e.target.value));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ fontSize: 50 }}>Counter</div>
+      <div>This is second</div>
+      <div>{counter}</div>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <div>
+        <input onChange={updateCounter} />
+      </div>
     </div>
   );
 }
